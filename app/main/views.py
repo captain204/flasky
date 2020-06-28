7ba1caf58c5770ca7d6ba70502ab47db5829b66c
captain204 <nurudeenakindele8@gmail.com>
2020-06-28 13:42:46 +0100

    migrations added

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from datetime import datetime
 from flask import render_template, session, redirect, url_for
 from . import main
-from .forms import NameForm
+#from .forms import NameForm
 from .. import db
 from ..models import User
 
